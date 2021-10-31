be29569cc098f67a76f167e5341f3e77ac8af574
Olga Bondarenko <olga.bondarenko@orioninc.com>
2021-10-31 19:47:04 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
+  const dispatch = useDispatch();
+  const cash = useSelector((state) => state.cash.cash);
+  console.log("cash", cash);
+
+  const addCash = (cash) => {
+    dispatch({ type: "ADD_CASH", payload: cash });
+  };
+
+  const getCash = (cash) => {
+    dispatch({ type: "GET_CASH", payload: cash });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{cash}</h1>
+      <div
+      //  style={{ display: "flex" }}
+      >
+        <button onClick={() => addCash(+prompt())}>Пополнить счет</button>
+        <button onClick={() => getCash(+prompt())}> Снять со счета</button>
+      </div>
     </div>
   );
 }
